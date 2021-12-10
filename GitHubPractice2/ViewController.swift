7268ad07d7f994f836d6cb5d1e2ffe29db4716e9
vincentDiliberto <vdiliberto3099@stu.d214.org>
2021-12-10 10:00:47 -0600

    Initial Commit

---
+++ GitHubPractice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice2
+//
+//  Created by Vincent Diliberto on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
