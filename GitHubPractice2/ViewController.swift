60a65ef69323c572531f504201084f164dc9cecb
Ashley Saju <ashley.saju67@gmail.com>
2021-12-10 10:10:33 -0600

    Viewdidload

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var animals = ["cheetah", "dog", "cat", "fish", "penguin", "elephant"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           <#code#>
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           <#code#>
+       }
 
 }
 
